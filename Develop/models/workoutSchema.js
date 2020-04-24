ebc03052abfbbd559d1b7d5948782b49cc198add
D3viii <devinsdevs@gmail.com>
2020-04-23 21:37:08 -0400

    Finished Schema

--- Develop/models/workoutSchema.js
+++ Develop/models/workoutSchema.js
@@ -12,3 +12,14 @@ const workoutSchema = new Schema(
     },
     { toJSON: { virtuals: true }}
 );
+
+workoutSchema.virtual("totalDuration").get(function () {
+    let totalDuration = 0;
+    for(var i = 0; i < this.exercises.length; i++){
+        totalDuration+=this.exercises[i].duration
+    }
+    return totalDuration
+});
+
+const workout = mongoose.model("workout", workoutSchema);
+module.exports = workout
